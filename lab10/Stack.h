4c9a2b1e70e97e38c012e88a48cb278298eaba1e
karsh <utkarsh@cyberfear.com>
2024-05-19 17:59:19 +1000

    solution

--- lab10/Stack.h
+++ lab10/Stack.h
@@ -14,61 +14,147 @@ private:
     size_t fStackPointer;
     size_t fCurrentSize;
 
-#ifdef P1
-	    
-    void resize( size_t aNewSize );
-    void ensure_capacity();
-    void adjust_capacity();
+    void resize( size_t aNewSize )
+    {
+        assert( fStackPointer <= aNewSize );
 
-#endif
-    
+        T* lNewElements = new T[aNewSize];
+
+        for ( size_t i = 0; i < fStackPointer; i++ )
+        {
+            lNewElements[i] = std::move( fElements[i] );
+        }
+
+        delete[] fElements;
+
+        fElements = lNewElements;
+        fCurrentSize = aNewSize;
+      }
+
+      void ensure_capacity()
+      {
+          // Is load factor 1?
+          if ( fStackPointer == fCurrentSize )
+          {
+              resize( fCurrentSize * 2 );
+          }
+      }
+
+      void adjust_capacity()
+      {
+          // Is load factor 1/4?
+          if ( fStackPointer <= fCurrentSize / 4 )
+          {
+              resize( fCurrentSize / 2 );
+          }
+      }
+
 public:
-    
-#ifdef P1
-	
-    Stack();
-    ~Stack();
 
-#endif
+    Stack() :
+        fElements(new T[1]),
+        fStackPointer(0),
+        fCurrentSize(1)
+    {}
 
-#ifdef P3
-	
-    Stack( const Stack& aOther ); 
-    
-    Stack& operator=( const Stack<T>& aOther );
+    ~Stack()
+    {
+        delete[] fElements;
+    }
 
-#endif
+    // copy semantics
+    Stack( const Stack& aOther ) :
+        fElements(new T[aOther.fCurrentSize]),
+        fStackPointer(aOther.fStackPointer),
+        fCurrentSize(aOther.fCurrentSize)
+    {
+        assert( fStackPointer <= fCurrentSize );
 
-#ifdef P4
+        // we only need to copy contents up to stack pointer
+        for ( size_t i = 0; i < fStackPointer; i++ )
+        {
+            fElements[i] = aOther.fElements[i];
+        }
+    }
 
-    Stack( Stack<T>&& aOther ) noexcept;
-    
-    Stack& operator=( Stack<T>&& aOther ) noexcept;
+    Stack operator=( const Stack<T>& aOther )
+    {
+        if ( this != &aOther )           // self-assignment check
+        {
+            this->~Stack();              // free this stack
 
-    void swap( Stack& aOther ) noexcept;
+            new (this) Stack( aOther );  // in-place new copy constructor
+        }
 
-#endif
-      
-#ifdef P1
-	  
-    size_t size() const noexcept;
-    
-    std::optional<T> top() noexcept;
+        return *this;
+    }
 
-    void push( const T& aValue );
+    // move semantics
+    Stack( Stack<T>&& aOther ) noexcept :
+        Stack()
+    {
+        swap( aOther ); // swap idiom
+    }
 
-#endif
-	
-#ifdef P2
-	
+    Stack operator=( Stack<T>&& aOther ) noexcept
+    {
+        if ( this != &aOther )
+        {
+            swap( aOther ); // swap idiom
+        }
+
+        return *this;
+    }
+
+    void swap( Stack& aOther ) noexcept
+    {
+        std::swap( fElements, aOther.fElements );
+        std::swap( fStackPointer, aOther.fStackPointer );
+        std::swap( fCurrentSize, aOther.fCurrentSize );
+    }
+
+    size_t size() const noexcept
+    {
+        return fStackPointer;
+    }
+
+    std::optional<T> top() noexcept
+    {
+        if ( fStackPointer > 0 )
+        {
+            return std::optional<T>( fElements[fStackPointer - 1] );
+        }
+        else
+        {
+            return std::optional<T>();
+        }
+    }
+
+    void push( const T& aValue )
+    {
+        ensure_capacity();
+
+        fElements[fStackPointer++] = aValue;
+    }
+
     template<typename... Args>
-    void emplace(Args&&... args);
+    void emplace(Args&&... args)  // uses type deduction
+    {
+        ensure_capacity();
 
-#endif
+        // free old entry
+        fElements[fStackPointer].~T();
 
-#ifdef P1
-	
-    void pop();
-	
-#endif
+        // placement new (matching constructor)
+        new ( &fElements[fStackPointer++]) T( std::forward<Args>(args)... );
+    }
+
+    void pop()
+    {
+        assert( fStackPointer > 0 );
+
+        fStackPointer--;
+
+        adjust_capacity();
+    }
 };
